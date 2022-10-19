52dbdc1b3d46eac08a08af7c3d0821595eabdeba
Towela <112038406+Towela3@users.noreply.github.com>
2022-10-19 18:41:52 -0400

    sketch here

---
+++ sketch/sketch.js
@@ -0,0 +1,25 @@
+var words = ['hey','how','are','you','?'];
+var colors = [
+  [10,20,30],
+  [40,50,60],
+  [70,80,90],
+  [100,120,130],
+  [200,210,220],
+];
+function setup() {
+  createCanvas(400, 400);
+  textAlign(CENTER,CENTER);
+  frameRate(2);
+}
+
+function draw() {
+  var currentIndex = frameCount % words.length;
+  var currentColor = colors[currentIndex];
+  var currentWord = words[currentIndex];
+  
+  background(currentColor);
+  
+  fill(0)
+  textSize(100);
+  text(currentWord, width/2, height/2);
+}
